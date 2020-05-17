b652419bec4c011d4425ebc40006588d556bc047
Hanitra <andritsalamaramaroson@gmail.com>
2020-05-17 19:40:26 +0200

    Classe Cercle modif

--- pglp_9.9/src/main/java/fr/uvsq/pglp_9/Cercle.java
+++ pglp_9.9/src/main/java/fr/uvsq/pglp_9/Cercle.java
@@ -5,10 +5,45 @@
  */
 package fr.uvsq.pglp_9;
 
+import java.awt.geom.Point2D;
+
 /**
  *
  * @author andri
  */
-public class Cercle {
+public class Cercle implements Formes{
     
+    private String name;
+    private int rayon;
+    private Point2D centre;
+    
+    /**
+     * 
+     * @param name
+     * @param rayon
+     * @param centre 
+     */
+    public Cercle(String name, int rayon, Point2D centre){
+        this.name = name;
+        this.rayon = rayon;
+        this.centre = centre;
+        
+    }
+   
+    
+    public void move(double dx, double dy) {
+        //centre.move(dx, dy);
+    }
+    
+    public String getName(){
+        return  name;
+    }
+    
+    public int getRayon(){
+        return rayon;
+    }
+    
+    public Point2D getCentre(){
+        return centre;
+    }
 }
